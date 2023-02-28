143f197c557f1cdb7e5cee4a2c78c5fd2be9c2ed
Stephen Taylor <stephen@viztech360.com>
2023-02-28 13:39:09 -0500

    testing

---
+++ test.rb
@@ -0,0 +1,17 @@
+sentence = 'To be or not to be'
+
+def string_lengths(sentence)
+  strings = sentence.split
+  lengths = []
+  counter = 1
+
+  until counter > strings.size do
+    word_length = strings[counter - 1].length
+    lengths.push(word_length)
+    counter += 1
+  end
+
+  lengths
+end
+
+p string_lengths(sentence)
