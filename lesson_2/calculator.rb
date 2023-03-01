7ade247bb8669d07caa24a7f03f6711f2b129937
Stephen Taylor <stephen@viztech360.com>
2023-03-01 10:24:35 -0500

    calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -14,14 +14,14 @@ number2 = Kernel.gets().chomp().to_i()
 Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
-if operator == '1'
-  result = number1 + number2
+result = if operator == '1'
+          number1 + number2
 elsif operator == '2'
-  result = number1 - number2
+          number1 - number2
 elsif operator == '3'
-  result = number1 * number2
+          number1 * number2
 elsif operator == '4'
-  result = number1.to_f() / number2.to_f()
+          number1.to_f() / number2.to_f()
 end
 
 Kernel.puts("The result is #{result}")
