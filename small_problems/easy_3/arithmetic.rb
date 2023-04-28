ca0aebb89b27f6321174e308c460c632f05eb62d
stephen <stephentayloremail@icloud.com>
2023-04-28 17:04:20 -0400

    ok

---
+++ small_problems/easy_3/arithmetic.rb
@@ -0,0 +1,11 @@
+puts "==> Enter the first number:"
+num_1 = gets.to_i
+puts "==> Enter the second number:"
+num_2 = gets.to_i
+
+puts "==> #{num_1} + #{num_2} = #{num_1 + num_2}"
+puts "==> #{num_1} - #{num_2} = #{num_1 - num_2}"
+puts "==> #{num_1} * #{num_2} = #{num_1 * num_2}"
+puts "==> #{num_1} / #{num_2} = #{num_1 / num_2}"
+puts "==> #{num_1} % #{num_2} = #{num_1 % num_2}"
+puts "==> #{num_1} ** #{num_2} = #{num_1 ** num_2}"
